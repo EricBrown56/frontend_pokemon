ae7fed75fb5d5bad2d8563c35a66433984e711a6
EricBrown56 <groove505@gmail.com>
2024-08-08 10:06:52 -0500

    updated to add return statement

--- poke_search.js
+++ poke_search.js
@@ -1,7 +1,8 @@
 async function fetchPokemonData(pokemonName) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
     const pokemonData = await response.json();
-    
+    console.log(pokemonData);
+    return pokemonData;
 }
 
 const pokemonName = document.getElementById('pokeName').value;
